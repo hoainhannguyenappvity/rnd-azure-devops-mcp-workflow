f2405f08e66edfb8560aa442a21efc6ad6097243
thienlamins99 <thienlam@appvity.com>
2026-01-09 09:43:59 +0700

    Create mcp server

--- mcp/azure-devops/index.mjs
+++ mcp/azure-devops/index.mjs
@@ -1,1 +1,38 @@
-console.log('Hello World!');
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
+import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
+import { communicationTools } from './tools/communication.mjs';
+
+//#region Register MCP Server
+const server = new McpServer({
+  name: 'My Simple Server',
+  version: '1.0.0',
+});
+// #endregion
+
+//#region Register Tools
+export const tools = [...communicationTools];
+
+tools.forEach((tool) =>
+  server.registerTool(
+    tool.name,
+    {
+      description: tool.description,
+      inputSchema: tool.inputSchema,
+    },
+    tool.handler
+  )
+);
+//#endregion
+
+async function main() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+  console.error('------------------------------------------------');
+  console.error('MCP Server is running...');
+  console.error('------------------------------------------------');
+}
+
+main().catch((error) => {
+  console.error('Error running server:', error);
+  process.exit(1);
+});
---
+++ mcp/azure-devops/tools/user-profile.mjs
@@ -0,0 +1,24 @@
+import { z } from 'zod';
+
+export const userProfileTools = [
+  {
+    name: 'greet',
+    description: 'Gửi lời chào đến ai đó',
+    inputSchema: z.object({
+      name: z.string(),
+    }),
+    handler: async ({ name }) => ({
+      content: [{ type: 'text', text: `Xin chào ${name} from MCP Server!` }],
+    }),
+  },
+  {
+    name: 'farewell',
+    description: 'Gửi lời tạm biệt đến ai đó',
+    inputSchema: z.object({
+      name: z.string(),
+    }),
+    handler: async ({ name }) => ({
+      content: [{ type: 'text', text: `Tạm biệt ${name} from MCP Server!` }],
+    }),
+  },
+];
